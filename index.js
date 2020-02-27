5940ef851f21d42aa31c0bb38498809d22c48306
Rafal Drag <rd@fss.cc>
2020-02-27 14:55:50 +0100

    cleaner implementation

--- index.js
+++ index.js
@@ -1,75 +1,16 @@
-const axios = require('axios')
-const { JSDOM } = require('jsdom')
+const { address, username, password } = require('./config.json')
 
-const { username, password, address } = require('./config.json')
+const SolisInverter = require('./lib/solis_inverter.js')
+const inverter = new SolisInverter(address, username, password)
 
-const client = axios.create({
-  baseURL: `http://${address}`,
-  auth: { username, password },
-  responseType: 'document'
+const onData = data => {
+  console.log('inverter data', data)
+}
+
+inverter.fetchData().then(data => {
+  onData(data)
+
+  setInterval(() => {
+    inverter.fetchData().then(onData)
+  }, 10000)
 })
-
-/**
- * @param {string} variableName
- * @return {RegExp}
- */
-const createRegexp = variableName => new RegExp(`var ${variableName} = "(.*)";`)
-
-/**
- * @param {String} variableName
- * @return {function(*=): string}
- */
-const extractString = variableName => scriptContents => {
-  const rx = createRegexp(variableName)
-  const res = rx.exec(scriptContents)
-  return res ? res[1].trim() : null
-}
-
-/**
- * @param {String} variableName
- * @return {function(*=): number}
- */
-const extractFloat = variableName => scriptContents => {
-  const rx = createRegexp(variableName)
-  const res = rx.exec(scriptContents)
-  return res ? parseFloat(res[1].trim()) : 0
-}
-
-/**
- * unused vars:
- *  cover_mid
- *  cover_ver
- *  cover_wmode
- *  cover_ap_ssid
- *  cover_ap_ip
- *  cover_ap_mac
- *  cover_sta_ssid
- *  cover_sta_rssi
- *  cover_sta_ip
- *  cover_sta_mac
- *  status_a
- *  status_b
- *  status_c
- */
-const extractors = new Map()
-extractors.set('serialNumber', extractString('webdata_sn'))
-extractors.set('firmwareMain', extractString('webdata_msvn'))
-extractors.set('firmwareSlave', extractString('webdata_ssvn'))
-extractors.set('inverterModel', extractString('webdata_pv_type'))
-extractors.set('powerNow', extractFloat('webdata_now_p'))
-extractors.set('powerToday', extractFloat('webdata_today_e'))
-extractors.set('powerTotal', extractFloat('webdata_total_e'))
-
-client.get('/status.html')
-  .then(resp => {
-    const doc = new JSDOM(resp.data)
-    const script = doc.window.document.documentElement.getElementsByTagName('script').item(1).text
-
-    const inverterInfo = {}
-
-    extractors.forEach((fn, key) => {
-      inverterInfo[key] = fn(script)
-    })
-
-    console.log(inverterInfo)
-  })
