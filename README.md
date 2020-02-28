4a6aad551030878c7b3e92cb13c08274d7751b56
Rafal Drag <rd@fss.cc>
2020-02-28 14:19:53 +0100

    cleaner implementation

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# solis-inverter
+# Solis inverter JSON API
 A simple NodeJS application to read Solis PV inverter data using HTTP interface
+
+Using HTTP interface, extracts and prints data in format as below:
+
+`{
+    inverter: {
+     model: '0123',
+     serial: '1234567890ABCDE',
+     firmwareMain: '0123',
+     firmwareSlave: '0122'
+   },
+   logger: {
+     serial: '1234567890',
+     version: 'MW_08_0501_1.58',
+     mode: 'STA',
+     ap: {
+        ssid: 'AP_1213456789',
+        ip: '1.1.1.1',
+        mac: 'ABABABABABAB'
+    },
+     sta: {
+       ssid: 'NETWORK_NAME',
+       ip: '2.2.2.2',
+       mac: 'AABBCCDDEEFF',
+       rssi: '99%'
+     }
+   },
+   remoteServer: { a: true, b: false },
+   power: 9999,
+   energy: { today: 0, total: 6 }
+ }`
+ 
+ Usage:
+ 
+`node index.js --address <INVERTER_IP_ADDRESS> --username <INVERTER_WEB_USERNAME> --password <INVERTER_WEB_PASSWORD> --port <LISTEN_PORT>`
--- index.js
+++ index.js
@@ -1,16 +1,55 @@
-const { address, username, password } = require('./config.json')
+const SolisInverterClient = require('./lib/solis_inverter_client.js')
+const { name } = require('./package.json')
 
-const SolisInverter = require('./lib/solis_inverter.js')
-const inverter = new SolisInverter(address, username, password)
-
-const onData = data => {
-  console.log('inverter data', data)
+const { address, username, password, port } = require('yargs').argv
+if (!address) {
+  console.error('port not given')
+  process.exit(1)
 }
 
-inverter.fetchData().then(data => {
-  onData(data)
+if (!port) {
+  console.error('port not given')
+  process.exit(1)
+}
 
-  setInterval(() => {
-    inverter.fetchData().then(onData)
-  }, 10000)
+const solis = new SolisInverterClient(address, username, password)
+
+let lastResponse = null
+let lastDate = null
+
+/**
+ * @param what
+ */
+const log = what => console.log([(new Date()).toISOString(), name, what].join(' '))
+
+const server = require('http').createServer((req, res) => {
+  log(`received request from ${req.connection.remoteAddress}`)
+
+  solis.fetchData()
+    .then(data => {
+      lastResponse = data
+      lastDate = new Date()
+
+      res.writeHead(200, { 'Last-Modified': lastDate.toString() })
+      res.end(JSON.stringify(lastResponse))
+    })
+    .catch(error => {
+      log(`got error: ${error}`)
+
+      if (lastResponse) {
+        res.writeHead(200, { 'Last-Modified': lastDate.toString() })
+        res.end(JSON.stringify(lastResponse))
+      } else {
+        res.writeHead(500)
+        res.end(JSON.stringify({ error: error.toString() }))
+      }
+    })
+})
+
+server.listen(port, err => {
+  if (err) {
+    log(`unable to listen on port ${port}: ${err}`)
+  } else {
+    log(`listening on port ${port}`)
+  }
 })
